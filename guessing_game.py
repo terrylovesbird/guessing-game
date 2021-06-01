7044e5357567f52196013546607bba8d765ec84f
terrylovesbird <tianlin.cai@gmail.com>
2021-06-01 15:49:50 -0500

    James & Tianlin solution for guesssing_game

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,27 @@
 class GuessingGame():
+
+    def __init__(self, answer):
+        self.answer = answer
+        self.is_correct = False
+
+    def guess(self,guess):
+
+        self.guess = guess
+        if self.answer < self.guess:
+            print('high')
+        elif self.answer > self.guess:
+            print('low')
+        elif self.answer == self.guess:
+            print('correct')
+            self.is_correct = True
+
+
+    def solved(self):
+        print(self.is_correct)
+
+   
+game = GuessingGame(50)
+
+game.guess(50)
+game.solved()
+
